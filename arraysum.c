d3e9ef1be52fb553487f823bb721ac075310ee48
yashijain1234 <73181774+yashijain1234@users.noreply.github.com>
2021-01-19 14:12:15 +0530

    Update and rename README.md to arraysum.c

---
+++ arraysum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,val,sum=0;
+    scanf("%d",&n);
+    while(n--)
+    {
+        scanf("%d",&val);
+        sum+=val;
+    }
+    printf("%i",sum);
+}
